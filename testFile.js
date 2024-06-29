4dfabe655bb38dbcf56c33e4f9c595fc67ac189f
Clay Mangum <clay@trinitasaccounting.com>
2024-06-29 17:37:21 -0400

    completeing more time complexity problems by using set.has() since it is O(1) compared to array.includes() which is O(n)

--- testFile.js
+++ testFile.js
@@ -29,44 +29,112 @@
 // Return k.
 
 
-function removeValue(array, value) {
-    let valueCount = 0;
-    if (array.length < 2) {
-        console.log('not an array')
+// function removeValue(array, value) {
+//     let valueCount = 0;
+//     if (array.length < 2) {
+//         console.log('not an array')
 
-    }
-    else {
-        for (let i = 0; i < array.length; i++) {
-            if (array[i] === value) {
-                array[i] = '_'
-            }
-            else {
-                valueCount = valueCount + 1
+//     }
+//     else {
+//         for (let i = 0; i < array.length; i++) {
+//             if (array[i] === value) {
+//                 array[i] = '_'
+//             }
+//             else {
+//                 valueCount = valueCount + 1
 
-            }
-        }
-        console.log(valueCount)
-        // console.log(array.sort())
+//             }
+//         }
+//         console.log(valueCount)
+//         // console.log(array.sort())
 
-    }
-}
+//     }
+// }
 
-removeValue(2, 3)
+// removeValue(2, 3)
 
 
-const removeValues = (array1, value1) => {
-    let valueCount1 = 0
+// const removeValues = (array1, value1) => {
+//     let valueCount1 = 0
+//     for (let i = 0; i < array1.length; i++) {
+//         if (array1[i] === value1) {
+//             array1[i] = '_'
+//         }
+//         else {
+//             valueCount1++
+
+//         }
+//     }
+//     console.log(valueCount1)
+//     console.log(array1.sort())
+// }
+
+// removeValues([0, 1, 2, 2, 3, 0, 4, 2], 2)
+
+
+
+
+// Question 3_____________________________________________________________________________________________________________
+// Given an array of positive integers nums and a positive integer target, return the minimal length of a 
+// subarray
+//  whose sum is greater than or equal to target. If there is no such subarray, return 0 instead.
+
+
+// function findSubArray(array, target) {
+// let newArray = [];
+// for(let i = 0; i < array.length; i++) {
+//     let newArray1 = [];
+
+// }
+//     let twoSumsArray = [];
+//     if (array.includes(target)) {
+//         return (console.log(target))
+//     }
+//     else if (!array.includes(target)) {
+//         for (let i = 0; i < array.length - 1; i++) {
+//             let sum = array[i] + array[i + 1];
+//             if (sum === target) {
+//                 twoSumsArray.push(array[i])
+//                 twoSumsArray.push(array[i + 1])
+//                 return (console.log([array[i], array[i + 1]]))
+//             }
+//         }
+//     }
+//     else if (twoSumsArray.length < 2) {
+//         for (let i = 0; i < array.length - 2; i++) {
+//             let sum = array[i] + array[i + 1] + array[i + 2];
+//             if (sum === target) {
+//                 return (console.log([array[i], array[i + 1], array[i + 2]]))
+//             }
+//         }
+//     }
+// }
+
+// findSubArray([1, 4, 4], 4)
+
+
+
+
+
+// Questions 4______________________________________________________________________
+// Given two arrays return true or false if the arrays contain a common element
+
+function commonElement(array1, array2) {
+    let set1 = new Set();
     for (let i = 0; i < array1.length; i++) {
-        if (array1[i] === value1) {
-            array1[i] = '_'
-        }
-        else {
-            valueCount1++
-
-        }
+        set1.add(array1[i])
     }
-    console.log(valueCount1)
-    console.log(array1.sort())
+    console.log(set1);
+
+    for (let j = 0; j < array2.length; j++) {
+        if (set1.has(array2[j])) {
+            return console.log(true)
+        }
+        // else {
+        //     return console.log(false)
+        // }
+    }
+    return console.log(false)
 }
 
-removeValues([0, 1, 2, 2, 3, 0, 4, 2], 2)
+commonElement([1, 2, 3], [4, 5, 3])
